3e33d36488a03b52af5192d162d3e583d7277083
João vitor timm <100783620+JaunTimm@users.noreply.github.com>
2024-09-04 21:17:51 -0300

    Pequenas atualizações Aula04

--- FDW-Projeto/app/(tabs)/index.tsx
+++ FDW-Projeto/app/(tabs)/index.tsx
@@ -8,10 +8,10 @@ import { ThemedView } from '@/components/ThemedView';
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: '#abc24e', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/logo_get1.png')}
           style={styles.reactLogo}
         />
       }>
